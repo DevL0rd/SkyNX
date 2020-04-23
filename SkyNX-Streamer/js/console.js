205a17db55d1287c446a5208fb64bae6e99ee82b
devl0rd <dmhzmxn@gmail.com>
2020-04-23 15:40:30 -0600

    added more status messages

--- SkyNX-Streamer/js/console.js
+++ SkyNX-Streamer/js/console.js
@@ -6,6 +6,10 @@ ipcRenderer.on('log', function (event, genHtml) {
         var fps = genHtml.split("fps= ")[1].split(" ")[0];
         var bitrate = genHtml.split("bitrate=")[1].split(" ")[0];
         $("#statusbartext").html("Framrate: " + fps + " Bitrate: " + bitrate);
+    } else if (genHtml.includes("Connection timed out") || genHtml.includes("Waiting for connection")) {
+        $("#statusbartext").html("Waiting for connection...");
+    } else if (genHtml.includes("streamerProcess process exited")) {
+        $("#statusbartext").html("Streamer stopped.");
     }
     clearTimeout(logTimeout);
     logTimeout = setTimeout(function () {
