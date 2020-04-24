30804dfba09fec3a68ef47c1f0af929adf386258
devl0rd <dmhzmxn@gmail.com>
2020-04-23 18:00:06 -0600

    fixed fps status

--- SkyNX-Streamer/js/console.js
+++ SkyNX-Streamer/js/console.js
@@ -3,7 +3,7 @@ ipcRenderer.on('log', function (event, genHtml) {
     $("#consoleContainer").append(genHtml);
     // $("#statusbartext").html("Console: " + genHtml);
     if (genHtml.includes("fps=")) {
-        var fps = genHtml.split("fps= ")[1].split(" ")[0];
+        var fps = genHtml.split("fps=")[1].split(" ")[0];
         var bitrate = genHtml.split("bitrate=")[1].split(" ")[0];
         $("#statusbartext").html("Framrate: " + fps + " Bitrate: " + bitrate);
         fpsChartData.datasets[0].data.push(parseInt(fps));
